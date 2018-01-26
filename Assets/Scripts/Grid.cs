fe756737c9259177f129e6c51d94c789e4accd92
develop32 <develop32@gmail.com>
2018-01-26 21:07:50 +0200

    Second commit

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int Width = 10;
+    public int Height = 10;
+    public float Spacing;
+
+    private List<Cell> _cells = new List<Cell>();
+
+    private void Awake()
+    {
+        var x0 = -Spacing * Width * 0.5f;
+        var y0 = -Spacing * Height * 0.5f;
+
+        for (int x = 0; x < Width; x++)
+            for (int y = 0; y < Height; y++)
+            {
+                var instance = GameObject.Instantiate(Prefab);
+            }
+    }
+}
