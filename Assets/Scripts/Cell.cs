053bca89297fc618660b55bf300798a7afdc2b8f
develop32 <develop32@gmail.com>
2018-01-27 12:35:53 +0200

    Flexible attack paths

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public enum CellStatus
 {
@@ -23,6 +24,8 @@ public class Cell : MonoBehaviour
     public int Value;
     public bool Visited;
 
+    public List<Cell> Temp = new List<Cell>();
+
     private void Awake()
     {
         var renderer = GetComponent<Renderer>();
@@ -30,6 +33,71 @@ public class Cell : MonoBehaviour
 
         Material = Material.Instantiate(renderer.material);
         renderer.material = Material;
+    }
+
+    public Cell Neighbour(int x, int y)
+    {
+        x += X;
+        y += Y;
+
+        if (x < 0 || x >= Grid.Width ||
+            y < 0 || y >= Grid.Height)
+            return null;
+
+        return Grid.Cells[x, y];
+    }
+
+    private void considerAttackOrigin(Cell cell, int range)
+    {
+        if (cell == null)
+            return;
+
+        if (!Grid.HasPath(cell))
+            return;
+
+        if (cell.Value + 1 >= range)
+            return;
+
+        Temp.Add(cell);
+    }
+
+    public Cell AttackOrigin(int range)
+    {
+        Temp.Clear();
+
+        considerAttackOrigin(Neighbour(-1, 0), range);
+        considerAttackOrigin(Neighbour(1, 0), range);
+        considerAttackOrigin(Neighbour(0, -1), range);
+        considerAttackOrigin(Neighbour(0, 1), range);
+
+        if (Temp.Count == 0)
+            return Origin;
+
+        var mouse = Input.mousePosition;
+        var v0 = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, 0));
+        var v1 = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, 1));
+
+        RaycastHit hit;
+        if (Physics.Raycast(v0, (v1 - v0).normalized, out hit))
+        {
+            float minDist = 0;
+            Cell closest = null;
+
+            foreach (var cell in Temp)
+            {
+                var dist = Vector3.Distance(cell.transform.position, hit.point);
+
+                if (dist < minDist || closest == null)
+                {
+                    closest = cell;
+                    minDist = dist;
+                }
+            }
+
+            return closest;
+        }
+        else
+            return Origin;
     }
 
     private void Update()
@@ -73,6 +141,13 @@ public class Cell : MonoBehaviour
                     var inPath = false;
 
                     var node = Grid.Hover;
+                    Cell attackOrigin = null;
+
+                    if (Grid.Hover != null && Grid.Hover.Status == CellStatus.enemy)
+                    {
+                        attackOrigin = Grid.Hover.AttackOrigin(Grid.HoverRange);
+                        node = attackOrigin;
+                    }
 
                     while (node != null)
                     {
