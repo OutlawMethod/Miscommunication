d85817869fe02bd6179bc16c754d7c5021031c96
develop32 <develop32@gmail.com>
2018-01-27 12:39:47 +0200

    Attack range desc

--- Assets/Scripts/CharacterDesc.cs
+++ Assets/Scripts/CharacterDesc.cs
@@ -7,6 +7,7 @@ public struct CharacterDesc
     public string Name;
     public int MaxRange;
     public int MaxLives;
+    public int AttackRange;
     public float ShiftDuration;
     public float AttackDuration;
     public float ReturnDuration;
