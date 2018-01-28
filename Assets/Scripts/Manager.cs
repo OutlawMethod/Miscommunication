36dc21000e720db4ef9c60a2bfc98c8d7acbcfbb
develop32 <develop32@gmail.com>
2018-01-28 09:40:14 +0200

    Smaller arena, cameras closer

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -73,8 +73,8 @@ public class Manager : MonoBehaviour
 
         for (int i = 0; i < Descriptions.Length; i++)
         {
-            placeCharacter(i, i + 1, 0, 0);
-            placeCharacter(i, Descriptions.Length - i, Grid.Height - 1, 1);
+            placeCharacter(i, i, 0, 0);
+            placeCharacter(i, Descriptions.Length - i - 1, Grid.Height - 1, 1);
         }
 
         Grid.ClearStatus();
